4ada27adb5569cef2a77240475cb1b808fb5473a
MJAlaeddini <javad1379.a@gmail.com>
2023-06-30 01:49:16 +0330

    add Nextjs and add homePage with hellow world

---
+++ hello-world/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+export default function Home() {
+  
+  return (
+    
+    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <head>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"></link>
+      </head>
+      <body>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+      </body>
+
+      <body>
+        <div className="ui container">
+          <div className="ui segment">
+            <h1 className="ui header">Hello World</h1>
+            <p>This is a Semantic UI example</p>
+          </div>
+        </div>
+      </body>
+  </main>
+  )
+}
